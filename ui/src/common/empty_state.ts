86a7a020397968bd5a4b289f8e511ce5acd3ca2e
gaogaotiantian <gaogaotiantian@hotmail.com>
2022-01-26 22:18:14 -0800

    Fix sidebarVisible for 0.15.0

--- ui/src/common/empty_state.ts
+++ ui/src/common/empty_state.ts
@@ -114,7 +114,7 @@ export function createEmptyState(): State {
     traceConversionInProgress: false,
 
     perfDebug: false,
-    sidebarVisible: true,
+    sidebarVisible: false,
     hoveredUtid: -1,
     hoveredPid: -1,
     hoverCursorTimestamp: Time.INVALID,
